dd1a89af60914dbdb385aba14fcbec5257a17324
家暐 莊 <amos6572@hotmail.com>
2021-06-07 17:11:53 +0800

    Day 38. 戰線擴張・模擬戰 — UBike 地圖 X Webpack 環境建構 - TypeScript Webpack Integration(UBike 地圖建構)

--- Day38_ubike-map-example/src/index.ts
+++ Day38_ubike-map-example/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+// src/index.ts
+
+import L from 'leaflet';
+import mapConfig from './map.config';
+
+
+const {
+    coordinate, // 台北市座標
+    zoomLevel, // 地圖預設縮放等級
+    tileLayerURL, // 地圖的底圖
+    containerID // 選擇 ID 為 'map' 的 HTML 元素
+}=mapConfig;
+
+
+// 建立 Leaflet 地圖個體，`map` 代表選擇 ID 為 'map' 的 HTML 元素
+const map=L.map(containerID);
+
+// 設定地圖要聚焦的座標與縮放等級
+map.setView(coordinate,zoomLevel);
+
+// 設定地圖的底圖並加到地圖的個體中
+L.tileLayer(tileLayerURL).addTo(map);
