b0620f5fe22752711a9eaa3f7f4d08a98076463d
家暐 莊 <amos6572@hotmail.com>
2021-06-08 14:17:39 +0800

    Day 39. 戰線擴張・模擬戰 — UBike 地圖 X 資料處理 - Data Processing using Type Alias

--- Day38_ubike-map-example/src/index.ts
+++ Day38_ubike-map-example/src/index.ts
@@ -2,6 +2,7 @@
 
 import L from 'leaflet';
 import mapConfig from './map.config';
+import fetchData from './fetchData';
 
 
 const {
@@ -9,14 +10,20 @@ const {
     zoomLevel, // 地圖預設縮放等級
     tileLayerURL, // 地圖的底圖
     containerID // 選擇 ID 為 'map' 的 HTML 元素
-}=mapConfig;
+} = mapConfig;
 
 
 // 建立 Leaflet 地圖個體，`map` 代表選擇 ID 為 'map' 的 HTML 元素
-const map=L.map(containerID);
+const map = L.map(containerID);
 
 // 設定地圖要聚焦的座標與縮放等級
-map.setView(coordinate,zoomLevel);
+map.setView(coordinate, zoomLevel);
 
 // 設定地圖的底圖並加到地圖的個體中
 L.tileLayer(tileLayerURL).addTo(map);
+
+// 使用 fetchData，由於 fetch 函數 回傳的是 Promise
+// 因此必須接上 .then 印出資料
+fetchData().then(data => {
+    console.log(data);
+});
